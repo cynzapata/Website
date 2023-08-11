c5cddfe56350941afba316da2b8d9dbedae94291
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-08-10 23:51:57 -0500

    added comments to all pages

--- website.js
+++ website.js
@@ -1,6 +1,8 @@
+//This class is a custom javascript file that contains script for the sidebar menu 
 let menu = document.querySelector('#menu-icon');
 let nav = document.querySelector('.nav');
 
+//When the sidebar menu icon is clicked, the list of websites will open
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('open');
